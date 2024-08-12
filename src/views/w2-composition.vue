5b460d16b2a0d0fb180523c67edce53b1592a35f
Sandra <a0960529965@gmail.com>
2024-08-12 23:46:55 +0800

    W2

--- src/views/w2-composition.vue
+++ src/views/w2-composition.vue
@@ -1,6 +1,25 @@
 <script setup>
 import { ref } from 'vue'
-</script>
-<template>123</template>
+const api = 'https://todolist-api.hexschool.io/'
+const singUp = ref({
+  email: '',
+  password: '',
+  nickname: ''
+})
+const signupButton = async () => {
+  try {
+    const res = await axios.post(`${api}/users/sign_up`)
+  } catch (error) {}
+}
+</script>å
+<template>
+  <div class="week2">
+    <h1>W2-串接 Todo List API</h1>
+    <input type="email" name="mail" placeholder="mail" v-model="singUp.email" />
+    <input type="text" name="password" placeholder="password" v-model="singUp.password" />
+    <input type="text" name="nickname" placeholder="nickname" v-model="singUp.nickname" />
+    <button type="button" @click="signupButton">註冊</button>
+  </div>
+</template>
 
 <style></style>
